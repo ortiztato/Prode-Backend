be839ba2ebd51854e8e672a3be6547bc00d9a0c7
unknown <ortiztato@gmail.com>
2022-11-11 10:31:35 -0300

    mod cors

--- server.js
+++ server.js
@@ -11,15 +11,23 @@ db('mongodb+srv://ortiztato:antorchaz@clusterprode.lzicw5t.mongodb.net/?retryWri
 
 
 var app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 router(app);
 
 app.use('/app', express.static('public'));
 
+
+
 app.listen(8080);
 /* app.listen(3000); */
 console.log('La aplicación está escuchando en http://localhost:3000');
 
-app.use(cors());
